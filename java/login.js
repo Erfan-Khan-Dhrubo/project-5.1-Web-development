302c6c0bc65446281c7942afc5233556d6a59c06
Erfan Khan <erfan.khan.cse@gmail.com>
2025-02-25 14:33:04 +0600

    Fixing some Problem

--- java/login.js
+++ java/login.js
@@ -1,15 +1,18 @@
-document.getElementById("login_btn").addEventListener("click", function () {
-  const mobileNum = document.getElementById("Mobile_num").value;
-  const pin = document.getElementById("pin").value;
-  const pinInt = parseInt(pin);
-  if (mobileNum.length === 11) {
-    if (pinInt === 1414) {
-      console.log("ho");
-      window.location.href = "https://www.youtube.com/";
+document
+  .getElementById("login_btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const mobileNum = document.getElementById("Mobile_num").value;
+    const pin = document.getElementById("pin").value;
+    const pinInt = parseInt(pin);
+    if (mobileNum.length === 11) {
+      if (pinInt === 1414) {
+        console.log("ho");
+        window.location.href = "main.html";
+      } else {
+        alert("Your Password doesn't match");
+      }
     } else {
-      alert("Your Password doesn't match");
+      alert("Your Mobile Number Should be 11 digits");
     }
-  } else {
-    alert("Your Mobile Number Should be 11 digits");
-  }
-});
+  });
